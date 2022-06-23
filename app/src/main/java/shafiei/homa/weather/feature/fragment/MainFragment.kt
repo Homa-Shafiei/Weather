18f494e55f2311c1c8a1ff3c297ebe1e7ab9f112
Homa Shafiei <hardware.engineer@yahoo.com>
2022-06-24 01:55:21 +0430

    initial commit

---
+++ app/src/main/java/shafiei/homa/weather/feature/fragment/MainFragment.kt
@@ -0,0 +1,19 @@
+package shafiei.homa.weather.feature.fragment
+
+import android.os.Bundle
+import android.view.View
+import shafiei.homa.weather.R
+
+
+class MainFragment : BaseFragment(R.layout.fragment_main), View.OnClickListener {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onClick(view: View?) {
+
+    }
+
+}
